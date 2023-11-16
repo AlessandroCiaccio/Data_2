7efa0f8bd69b23306fdb413101259a6c84f7783a
AlessandroCiaccio <146941441+AlessandroCiaccio@users.noreply.github.com>
2023-11-16 12:49:38 +0100

    Updated Main

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 public class Main {
-    public static void main(String[] args){
-        OffsetDateTime data=OffsetDateTime.parse("2023-03-01T13:00:00Z");
-        String dataString= data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String dataString = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
         System.out.println(dataString);
     }
 }
